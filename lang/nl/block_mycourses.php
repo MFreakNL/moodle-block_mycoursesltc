d4eeb118bed91b7b91bad78e1fd54962b571d962
Luuk Verhoeven <luuk@ldesign.nl>
2019-11-27 17:24:23 +0100

    Add NL language file

---
+++ lang/nl/block_mycourses.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang "NL"
+ *
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @package   moodle-block_mycourses
+ * @copyright 28/10/2019 Mfreak.nl | LdesignMedia.nl - Luuk Verhoeven
+ * @author    Luuk Verhoeven
+ **/
+
+// Default.
+$string['pluginname'] = 'My courses';
+$string['description'] = 'Overview of an user there active courses';
+
+// Access.
+$string['mycourses:addinstance'] = 'Add a new block for user mycourses';
+$string['mycourses:myaddinstance'] = 'Add a new Block for user mycourses to my Moodle';
+$string['mycourses:view'] = 'Allow viewing block my courses';
+
+// Settings.
+$string['settings:defaultimage'] = 'Default image';
+$string['settings:defaultimagedesc'] = 'The default course images';
+$string['settings:courselimit'] = 'Course limit';
+$string['settings:courselimit_desc'] = 'The maximum number of enrolled courses shown';
+
+// Privacy.
+$string['privacy:metadata'] = 'The atto_clear plugin does not store any personal data.';
+
+// Text.
+$string['text:startdate'] = 'Startdate:';
+$string['text:grade'] = 'Grade:';
+$string['text:course_not_started'] = 'Not started';
+$string['text:course_finished'] = 'Completed (100%) on {$a->date}';
+$string['text:course_finished_no_date'] = 'Completed (100%)';
+
+// Button.
+$string['btn:start_course'] = 'Enter course';
+$string['btn:showmore'] = 'Show all';
